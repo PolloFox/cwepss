499addc4756ee543127f6171d0c65377feb33a55
Paul-André Duchesne <PolloFox@gmail.com>
2017-01-22 19:35:25 +0100

    Brought basis of a js. Functional for the moment but will need to include it in Drupal.behaviors

---
+++ web/themes/custom/cwepss_BS/js/cwepss.js
@@ -0,0 +1,20 @@
+/**
+ * Created by duchesne on 22/01/17.
+ */
+(function ($, Drupal) {
+    /*global jQuery:false */
+    /*global Drupal:false */
+    "use strict";
+    $( document ).ready(
+        function() {
+            $('a.accordion-open-close').on(
+                'click',
+                function(event) {
+                    var arrowCurrentClass = ($(this).find('span.glyphicon').hasClass('glyphicon-menu-down'))?'glyphicon-menu-down':'glyphicon-menu-up';
+                    var arrowTargetClass = ($(this).find('span.glyphicon').hasClass('glyphicon-menu-down'))?'glyphicon-menu-up':'glyphicon-menu-down';
+                    $(this).find('span.glyphicon').removeClass(arrowCurrentClass).addClass(arrowTargetClass);
+                }
+            );
+        }
+    );
+})(window.jQuery, window.Drupal);
